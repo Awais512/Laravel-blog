60c59c944256350e90a81bf7eb9c54188bf58649
awais512 <raza.awais25@gmail.com>
2019-08-23 17:02:29 +0500

    Delete Categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -93,6 +93,8 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return redirect('categories');
     }
 }
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,6 +4,13 @@
 
 <div class="container-fluid">
     <h1>{{$category->name}}</h1>
-    <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-warning btn-sm btn-margin-right">Edit</a>
+    <div class="btn-group">
+        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-warning btn-sm btn-margin-right">Edit</a>
+        <form action="{{ route('categories.destroy', $category->id) }}" method="POST">
+            {{method_field('delete')}}
+
+            <button class="btn btn-danger btn-sm pull-left btn-margin-right" type="submit">Delete</button> {{csrf_field()}}
+        </form>
+    </div>
 </div>
 @endsection
